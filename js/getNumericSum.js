48e213bf51258867f5602d1537b479011c267af5
skybaros <rostyslavsmile@gmail.com>
2016-02-11 17:48:29 +0200

    Edited homework

--- js/getNumericSum.js
+++ js/getNumericSum.js
@@ -1,25 +1,4 @@
-"Use strict;";
+"use strict;";
 (function() {
-    function getNumbers(arr) {
-        var sum = 0;
-        var newarr = [];
-        for (i = 0; i < arr.length; i++) {
-            arr[i] = parseFloat(arr[i]);
-            if (!isNaN(arr[i])) {
-                newarr.push(arr[i]);
-            }
-        }
-        console.log(newarr);
-    }
-    function getNumericSum(arr) {
-        getNumbers(arr);
-        var sum = 0;
-        for (i = 0; i <arr.length; i++) {
-            if (!isNaN(arr[i])) {
-                sum = sum + arr[i];
-            }
-        }
-        console.log("Result -> "+sum);
-    }
-    getNumericSum(['4', '1', 'hi', 'gtg']);
+    
 })();
--- js/getPensionersName.js
+++ js/getPensionersName.js
@@ -1,36 +1,4 @@
 "use strict;";
 (function() {
-  function getPensionersName(obj) {
-    var arr = [];
-    for (var i = 0; i < obj.length; i++) {
-      console.log(obj[i].name + " is " + obj[i].age + " years old");
-      if (obj[i].age > 65) {
-        arr.push(obj[i].name);
-
-      }
-    }
-    console.log("Result -> " + arr);
-  }
-
-  var human = [{
-    name: "John",
-    age: 50
-  }, {
-    name: "Dick",
-    age: 65
-  }, {
-    name: "Ros",
-    age: 21
-  }, {
-    name: "Mat",
-    age: 95
-  }, {
-    name: "Doe",
-    age: 66
-  }, {
-    name: "Taisa",
-    age: 16
-  }];
-
-  getPensionersName(human);
+ 
 })();
